9c2ceb17116e71c8fbfe0eb9cb3767edd67d2bd6
Josh Johnson <josh@jshjohnson.com>
2014-02-02 22:54:05 +0000

    Removed BG div from index and 404

--- content/themes/TARVA/404.php
+++ content/themes/TARVA/404.php
@@ -1,8 +1,5 @@
 <?php get_header(); ?>
-		<div class="content__bg">
-			<div class="content__container container">		
-				<article><?php get_template_part( 'parts/not-found' ); ?>	</article>
-			</div>
+		<div class="content__container container">		
+			<article><?php get_template_part( 'parts/not-found' ); ?></article>
 		</div>
-<?php get_sidebar(); ?> 
 <?php get_footer(); ?>
--- content/themes/TARVA/index.php
+++ content/themes/TARVA/index.php
@@ -1,16 +1,14 @@
 <?php get_header(); ?>
-		<div class="content__bg">
-			<div class="content__container container">		
-				<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<article class="content__body">
-					<?php the_content(); ?>
-				</article>	
-				<?php endwhile; ?>
-				<?php else : ?>
+		<div class="content__container container">		
+			<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<article class="content__body">
+				<?php the_content(); ?>
+			</article>	
+			<?php endwhile; ?>
+			<?php else : ?>
 <?php get_template_part( 'parts/not-found' ); ?>		
-				<?php endif; ?>
-			</div>
+			<?php endif; ?>
 		</div>
 <?php get_sidebar(); ?> 
 <?php get_footer(); ?>
