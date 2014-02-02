188c4b3b1d301e31aae393cbb3cf1208125dc460
Josh Johnson <josh@jshjohnson.com>
2014-02-02 20:36:44 +0000

    Added pages order plugin // Added underline to single article h2's

--- content/themes/TARVA/index.php
+++ content/themes/TARVA/index.php
@@ -3,7 +3,7 @@
 			<div class="content__container container">		
 				<?php if ( have_posts() ) : ?>
 				<?php while ( have_posts() ) : the_post(); ?>
-				<article>
+				<article class="content__body">
 					<?php the_content(); ?>
 				</article>	
 				<?php endwhile; ?>
