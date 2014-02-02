52f5d3dd4639d795203b0428cd3aa1bf96b02e29
Josh Johnson <josh@jshjohnson.com>
2014-02-02 23:11:36 +0000

    Begun grabbing data the opposite way for single centre pages

--- content/themes/TARVA/single-centre.php
+++ content/themes/TARVA/single-centre.php
@@ -3,16 +3,11 @@
 		<?php if ( have_posts() ) : ?>
 		<article class="bio module">
 			<?php while ( have_posts() ) : the_post(); ?>
-			<img src="<?php the_field('avatar');?>" alt="">
 			<dl class="module__split">
 				<dt>Name:</dt>
 				<dd><?php the_title(); ?></dd>
-				<dt>Job title:</dt>
-				<dd><?php the_field('job_title'); ?></dd>
-				<dt>Role:</dt>
-				<dd><?php the_field('job_role'); ?></dd>
 				<?php 
-				$post_object = get_field('hospital');
+				$post_object = get_field('primary_investigator');
 
 				if( $post_object ): 
 
@@ -21,24 +16,20 @@
 				setup_postdata( $post ); 
 
 				?>
-				<dt>Hospital:</dt>
+				<dt>Primary Investigator:</dt>
 				<dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
-				<dt>Address:</dt>
-				<dd><?php the_field('address'); ?></dd>
+				<dt>Co-investigators:</dt>
+				<dd><?php the_field('co-investigators'); ?></dd>
+				<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+				<?php endif; ?>
+				<dt>Research Coordinator:</dt>
+				<dd><?php the_field('coordinator'); ?></dd>
 				<dt>Tel:</dt>
 				<dd><?php the_field('telephone'); ?></dd>
 				<dt>Email:</dt>
 				<dd><a href="mailto:<?php the_field('email'); ?>"><?php the_field('email'); ?></a></dd>
 				<dt>Website:</dt>
 				<dd><a href="<?php the_field('website'); ?>"><?php the_field('website'); ?></a></dd>
-				<?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-				<?php endif; ?>
-				<dt>Bio:</dt>
-				<dd><?php the_field('bio'); ?></dd>
-				<dt>Appointments:</dt>
-				<dd><?php the_field('appointments'); ?></dd>
-				<dt>Private appointments:</dt>
-				<dd><?php the_field('private_appointments'); ?></dd>
 			</dl>
 			<?php endwhile; ?>
 		</article>
--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -1,14 +1,12 @@
 <?php get_header(); ?>
-	<div class="content__bg">
-		<div class="content__container container">
-			<?php if ( have_posts() ) : ?>
-			<article class="content__body">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php the_content(); ?>
-				<?php endwhile; ?>
-				<?php disqus_embed('tarva'); ?>
-			</article>
-			<?php endif; ?>
-		</div>
+	<div class="content__container container">
+		<?php if ( have_posts() ) : ?>
+		<article class="content__body">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+			<?php disqus_embed('tarva'); ?>
+		</article>
+		<?php endif; ?>
 	</div>
 <?php get_footer(); ?>
