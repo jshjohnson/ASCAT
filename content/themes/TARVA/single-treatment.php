e6efbb81ab3101d6816fd8177b18fad20d145503
Josh Johnson <josh@jshjohnson.com>
2014-02-03 16:26:28 +0000

    Added next and previous links to treatment pages // Removed bg from single template

--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -1,4 +1,10 @@
 <?php get_header(); ?>
+	<?php if(get_next_post()) : ?>
+	<div class="credit zero-top right"><?php next_post(); ?></div>
+	<?php endif; ?>
+	<?php if(get_previous_post()) : ?>
+	<div class="credit zero-top left"><?php previous_post(); ?></div>
+	<?php endif; ?>
 	<div class="content__container container">
 		<?php if ( have_posts() ) : ?>
 		<article class="content__body">
--- content/themes/TARVA/single.php
+++ content/themes/TARVA/single.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
-		<div class="content__bg">
-			<div class="content__container container">		
-				<?php if ( have_posts() ) : ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<article>
-					<h5><?php the_time( 'jS F Y' ); ?></h5>
-					<h5><?php the_category( ', ' ); ?></h5>
-					<?php the_content(); ?>
-				</article>	
-				<?php endwhile; ?>
-				<?php else : ?>
+		<div class="content__container container">		
+			<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<article>
+				<h5><?php the_time( 'jS F Y' ); ?></h5>
+				<h5><?php the_category( ', ' ); ?></h5>
+				<?php the_content(); ?>
+			</article>	
+			<?php endwhile; ?>
+			<?php else : ?>
 <?php get_template_part( 'parts/not-found' ); ?>		
-				<?php endif; ?>
-			</div>
+			<?php endif; ?>
 		</div>
 <?php get_sidebar(); ?> 
 <?php get_footer(); ?>
