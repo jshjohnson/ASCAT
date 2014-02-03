fafd6d60bb8da8dbc23ccaff41e374abe73d6acf
Josh Johnson <josh@jshjohnson.com>
2014-02-03 18:05:09 +0000

    Change format of next and previous links

--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
-	<?php if(get_next_post()) : ?>
-	<div class="nav--page right"><?php next_post(); ?></div>
+	<?php if(get_next_post_link()) : ?>
+	<div class="nav--page right"><?php next_post_link('Next treatment: <strong>%link</strong>'); ?> </div>
 	<?php endif; ?>
-	<?php if(get_previous_post()) : ?>
-	<div class="nav--page left"><?php previous_post(); ?></div>
+	<?php if(get_previous_post_link()) : ?>
+	<div class="nav--page left"><?php previous_post_link('Previous treatment: <strong>%link</strong>'); ?></div>
 	<?php endif; ?>
 	<div class="content__container content__container--less-marg container">
 		<?php if ( have_posts() ) : ?>
