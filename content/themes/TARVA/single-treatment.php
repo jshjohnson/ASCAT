6941a81a16b106816fa126b7a87321e48795b491
Josh Johnson <josh@jshjohnson.com>
2014-02-03 16:36:04 +0000

    Next - prev link styling

--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 	<?php if(get_next_post()) : ?>
-	<div class="credit zero-top right"><?php next_post(); ?></div>
+	<div class="nav--page right"><?php next_post(); ?></div>
 	<?php endif; ?>
 	<?php if(get_previous_post()) : ?>
-	<div class="credit zero-top left"><?php previous_post(); ?></div>
+	<div class="nav--page left"><?php previous_post(); ?></div>
 	<?php endif; ?>
-	<div class="content__container container">
+	<div class="content__container content__container--less-marg container">
 		<?php if ( have_posts() ) : ?>
 		<article class="content__body">
 			<?php while ( have_posts() ) : the_post(); ?>
