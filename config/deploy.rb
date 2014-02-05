d462942c740c99923ab8b406771f032d79a55896
Josh Johnson <josh@jshjohnson.com>
2014-02-05 21:40:20 +0000

    Capifying from scratch

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,15 @@
-# 
-#  After running cap deploy:cold, You'll need to remove all files from the 
-#  domains/yourdomain.com/html directory and turn the html directory into a 
+require 'capistrano-deploytags'
+ 
+set :stages, %w(prd dev)
+set :default_stage, 'dev'
+ 
+require 'capistrano/ext/multistage'
+ 
+#
+#  After running cap deploy:cold, You'll need to remove all files from the
+#  domains/yourdomain.com/html directory and turn the html directory into a
 #  symlink that links to ./current which is also a symlink setup by capistrano.
 #
- 
-# install the mt-capistrano gem, not sure if it is really needed in most situations
-require 'mt-capistrano' 
  
 # Configure these
 set :site,         "156312" # this is your site number, for example if your access domain is s12345.gridserver.com
@@ -48,16 +52,21 @@ def relative_path(from_str, to_str)
   require 'pathname' 
   Pathname.new(to_str).relative_path_from(Pathname.new(from_str)).to_s 
 end 
- 
+
 # overwrite the symlink method to use the relative path method above
-namespace :deploy do 
-  desc "Relative symlinks for current, so we don't use full path" 
-  task :symlink, :except => { :no_release => true } do 
-    if releases[-2] # not the first release 
-      previous_release_relative = relative_path(deploy_to, previous_release) 
-      on_rollback { run "rm -f #{current_path}; ln -s #{previous_release_relative} #{current_path}; true" } 
-    end 
-    latest_release_relative = relative_path(deploy_to, latest_release) 
-    run "rm -f #{current_path} && ln -s #{latest_release_relative} #{current_path}" 
+namespace :deploy do
+  desc "Relative symlinks for current, so we don't use full path"
+  task :create_symlink, :except => { :no_release => true } do
+    if releases[-2] # not the first release
+      previous_release_relative = relative_path(deploy_to, previous_release + '/content')
+      on_rollback { run "rm -f #{current_path}; ln -s #{previous_release_relative} #{current_path}; true" }
+    end
+    latest_release_relative = relative_path(deploy_to, latest_release + '/content')
+    run "rm -f #{current_path} && ln -s #{latest_release_relative} #{current_path}"
   end
+end
+ 
+after "deploy:finalize_update" do
+  redmonster.symlinks
+  run "cp #{current_release}/wp-config-#{stage}.php #{current_release}/wp-config.php"
 end
