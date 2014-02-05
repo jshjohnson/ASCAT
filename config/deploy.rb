6f628fe4882b813e69522a7f3767c8bf0773516a
Josh Johnson <josh@jshjohnson.com>
2014-02-05 23:10:42 +0000

    Media temple relative path

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,9 +44,36 @@ set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules", "capfile",
 set :use_sudo, false
 ssh_options[:forward_agent] = true
 
+# we need a relative path for the current symlink, without this
+# current is set to link to the release starting from the /home directory
+# which has a directory that is not owned by the serveradmin and apache
+# won't have access
+def relative_path(from_str, to_str)
+  require 'pathname'
+  Pathname.new(to_str).relative_path_from(Pathname.new(from_str)).to_s
+end
+
 ############################################
 # Recipies
 ############################################
+
+# overwrite the symlink method to use the relative path method above
+namespace :deploy do
+  desc "Relative symlinks for current, so we don't use full path"
+  task :create_symlink, :except => { :no_release => true } do
+    if releases[-2] # not the first release
+      previous_release_relative = relative_path(deploy_to, previous_release + '/htdocs')
+      on_rollback { run "rm -f #{current_path}; ln -s #{previous_release_relative} #{current_path}; true" }
+    end
+    latest_release_relative = relative_path(deploy_to, latest_release + '/htdocs')
+    run "rm -f #{current_path} && ln -s #{latest_release_relative} #{current_path}"
+  end
+end
+ 
+after "deploy:finalize_update" do
+  redmonster.symlinks
+  run "cp #{current_release}/htdocs/wp-config-#{stage}.php #{current_release}/htdocs/wp-config.php"
+end
 
 ### WordPress
 
