f6e0806e5a71445b5187bbc8b7848fa47216256a
Josh Johnson <josh@jshjohnson.com>
2014-02-06 20:03:07 +0000

    Add post type to search // Overflow ellipsis for date and blog title

--- content/themes/TARVA/search-specialists.php
+++ content/themes/TARVA/search-specialists.php
@@ -7,6 +7,7 @@ Template Name: Search Specialists
 		<div class="content__container container">
 			<form class="search cf" method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
 				<label for="s"><?php _e('Find a specialist'); ?></label>
+				<input type="hidden" name="post_type" value="investigator">
 				<input type="search" value="<?php echo trim( get_search_query() ); ?>" name="s" id="s" placeholder="Search the site" required>
 				<input class="submit" name="submit" type="submit" value='Search'>	
 			</form>   		
