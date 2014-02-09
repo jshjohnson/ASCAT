d8915ded974733c6f908e88abd78d29b74c5a728
Josh Johnson <josh@jshjohnson.com>
2014-02-09 14:32:24 +0000

    Reduced avatar size <bp3 // Added fallback to search pages

--- content/themes/TARVA/search.php
+++ content/themes/TARVA/search.php
@@ -20,7 +20,11 @@
 				?>
 					<article class="grid__cell unit-1-2--bp2">
 						<div class="bio island">
-							<img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>">
+							<?php if($url) : ?>
+							<img class="bio__avatar" src="<?php echo $url; ?>" alt="<?php echo $alt; ?>">
+							<?php else : ?>
+							<img class="bio__avatar" src="http://localhost:8888/TARVA/content/uploads/2014/02/avatar-fallback-256x300.png" alt="Avatar">
+							<?php endif; ?>
 							<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>"><?php echo $title; ?></a></h2>
 							<h3 class="listing-subtitle"><?php the_field('job_role'); ?></h3>
 							<p><?php the_field('bio'); ?></p>
