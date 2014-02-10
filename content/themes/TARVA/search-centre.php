9bb3e51163e08988633443df61e97e50ca75bed1
Josh Johnson <josh@jshjohnson.com>
2014-02-10 22:35:02 +0000

    Temporarily remove img from centre

--- content/themes/TARVA/search-centre.php
+++ content/themes/TARVA/search-centre.php
@@ -47,7 +47,6 @@ Template Name: Search Centres
 				?>
 					<article class="grid__cell unit-1-2--bp2">
 						<div class="bio hospital island">
-							<img src="<?php echo $url; ?>" alt="<?php echo $alt; ?>">
 							<h2 class="listing-title"><a href="<?php the_permalink(); ?>"><?php echo $title; ?></a></h2>
 							<h3 class="listing-subtitle"><?php the_field('location'); ?></h3>
 							<a class="more-link" href="<?php the_permalink(); ?>">More</a>
