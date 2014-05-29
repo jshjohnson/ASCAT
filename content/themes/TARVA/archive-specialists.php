9e520f6e0318fc579237779d689e4547eea443c5
Josh Johnson <josh@jshjohnson.com>
2014-05-29 08:48:21 +0100

    Specialist listing structure

--- content/themes/TARVA/archive-specialists.php
+++ content/themes/TARVA/archive-specialists.php
@@ -122,9 +122,11 @@ Template Name: Search Specialists
 								<?php else : ?>
 								<img class="bio__avatar" src="<?php echo home_url(); ?>/content/uploads/2014/02/avatar-fallback-256x300.png" alt="Avatar">
 								<?php endif; ?>
-								<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>"><?php echo $title; ?></a></h2>
-								<h3 class="listing-subtitle"><?php the_field('job_title'); ?></h3>
-								<a class="more-link" href="<?php the_permalink(); ?>">More</a>
+								<div class="bio__content">
+									<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>"><?php echo $title; ?></a></h2>
+									<h3 class="listing-subtitle listing-subtitle--bio"><?php the_field('job_title'); ?></h3>
+									<a class="more-link" href="<?php the_permalink(); ?>">More</a>
+								</div>
 							</div>
 						</article>
 					<?php endwhile; ?>
