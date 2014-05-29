e447add6141ed96096aab664421f7b8cce9daa92
Josh Johnson <josh@jshjohnson.com>
2014-05-29 12:59:18 +0100

    Added recommendation section to single treatments

--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -5,9 +5,15 @@
 	<?php if(get_previous_post_link()) : ?>
 	<div class="nav--page"><?php previous_post_link('Previous treatment: <strong>%link</strong>'); ?></div>
 	<?php endif; ?>
-	<div class="content__container container">
+	<div class="content__container content__container--less-marg  container">
 		<?php if ( have_posts() ) : ?>
 		<article class="content__body">
+		<?php $recommendation = get_field('recommendation'); if($recommendation) : ?>
+			<div class="message message--highlight">
+				<h2 class="message__title">Recommendation</h2>
+				<?php echo $recommendation; ?>
+			</div>
+		<?php endif;?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
