98752ff270ce879750cd63ae7a5df3dabc100d8b
Josh Johnson <josh@jshjohnson.com>
2014-05-29 13:14:22 +0100

    Move recommendation to footer

--- content/themes/TARVA/single-treatment.php
+++ content/themes/TARVA/single-treatment.php
@@ -8,15 +8,16 @@
 	<div class="content__container content__container--less-marg  container">
 		<?php if ( have_posts() ) : ?>
 		<article class="content__body">
-		<?php $recommendation = get_field('recommendation'); if($recommendation) : ?>
-			<div class="message message--highlight">
-				<h2 class="message__title">Recommendation</h2>
-				<?php echo $recommendation; ?>
-			</div>
-		<?php endif;?>
+		<?php global $mwm_aal; echo $mwm_aal->output_content_links(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
 			<?php endwhile; ?>
+			<?php $recommendation = get_field('recommendation'); if($recommendation) : ?>
+			<div class="message--highlight message--flipped">
+				<h2 class="message__title">Recommendation</h2>
+				<?php echo $recommendation; ?>
+			</div>
+			<?php endif;?>
 			<?php disqus_embed('tarva'); ?>
 		</article>
 		<?php endif; ?>
