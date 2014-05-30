866b3cc88214aeea1d2217ddd38a4a283378fe1a
Josh Johnson <josh@jshjohnson.com>
2014-05-30 18:09:08 +0100

    Turn drag off of google map

--- content/plugins/wp-store-locator/js/wpsl-gmap.js
+++ content/plugins/wp-store-locator/js/wpsl-gmap.js
@@ -242,6 +242,7 @@ function initializeGmap() {
 		mapTypeId: mapType,
 		mapTypeControl: false,
 		scrollwheel: false,
+		draggable: false
 		panControl: false,
 		streetViewControl: streetViewVisible,
 			zoomControlOptions: {
