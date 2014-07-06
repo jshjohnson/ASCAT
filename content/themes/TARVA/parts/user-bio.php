93ae547e6f18b103d8a267c4d14d7515f3bd6eee
Josh Johnson <josh@jshjohnson.com>
2014-07-06 20:06:49 +0100

    Class renaming

--- content/themes/TARVA/parts/user-bio.php
+++ content/themes/TARVA/parts/user-bio.php
@@ -6,12 +6,12 @@
 								<img class="bio__avatar" src="<?php echo home_url(); ?>/content/uploads/2014/02/avatar-fallback-256x300.png" alt="Avatar">
 								<?php endif; ?>
 								<div class="bio__content">
-									<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>" class="more-link"><?php echo $title; ?></a></h2>
+									<h2 class="listing__title listing__title--bio"><a href="<?php the_permalink(); ?>" class="more-link"><?php echo $title; ?></a></h2>
 								<?php $jobTitle = get_field('job_title'); if($jobTitle) : ?>
-									<h3 class="listing-subtitle listing-subtitle--bio"><?php echo $jobTitle; ?></h3>
+									<h3 class="listing__subtitle listing__subtitle--bio"><?php echo $jobTitle; ?></h3>
 								<?php endif; ?>
 								<?php $role = get_field('committee_role'); if($role) : ?>
-									<h3 class="listing-subtitle listing-subtitle--bio block-title"><?php echo $role; ?></h3>
+									<h3 class="listing__subtitle listing__subtitle--bio block-title"><?php echo $role; ?></h3>
 								<?php endif; ?>
 								</div>
 							</div>
