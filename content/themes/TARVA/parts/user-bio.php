95c53d9da37371bdf37b2f4e7470a8acee3893f3
Josh Johnson <josh@jshjohnson.com>
2014-07-06 13:30:09 +0100

    Remove more link on user bios -> title is link with icon

--- content/themes/TARVA/parts/user-bio.php
+++ content/themes/TARVA/parts/user-bio.php
@@ -6,14 +6,13 @@
 								<img class="bio__avatar" src="<?php echo home_url(); ?>/content/uploads/2014/02/avatar-fallback-256x300.png" alt="Avatar">
 								<?php endif; ?>
 								<div class="bio__content">
-									<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>"><?php echo $title; ?></a></h2>
+									<h2 class="listing-title listing-title--bio"><a href="<?php the_permalink(); ?>" class="more-link"><?php echo $title; ?></a></h2>
 								<?php $jobTitle = get_field('job_title'); if($jobTitle) : ?>
 									<h3 class="listing-subtitle listing-subtitle--bio"><?php echo $jobTitle; ?></h3>
 								<?php endif; ?>
 								<?php $role = get_field('committee_role'); if($role) : ?>
 									<h3 class="listing-subtitle listing-subtitle--bio block-title"><?php echo $role; ?></h3>
 								<?php endif; ?>
-									<p><a class="more-link" href="<?php the_permalink(); ?>">More</a></p>
 								</div>
 							</div>
 						</article>
