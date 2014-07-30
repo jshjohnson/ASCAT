fbe2374c38287a7d7fa4e35d334606a78bf8d786
Josh Johnson <josh@jshjohnson.com>
2014-07-30 22:24:19 +0100

    Housekeeping

--- content/themes/TARVA/assets/js/scripts.js
+++ content/themes/TARVA/assets/js/scripts.js
@@ -15,15 +15,12 @@
 
             app.sanitiseWP.init();
             app.modernizrSVG.init();
-            app.modernizrPlaceholder.init();
-            app.smoothScroll.init();
 
         };
 
         return app;
     })();
 })(jQuery, window, window.document);
-
 
 // Sanitise WP content
 (function($, window, document, app, undefined) {
@@ -39,7 +36,6 @@
         return module;
     })();
 })(jQuery, window, window.document, window.App);
-
 
 // Modernizr SVG image polyfill
 (function($, window, document, app, undefined) {
@@ -62,66 +58,6 @@
     })();
 })(jQuery, window, window.document, window.App);
 
-// Modernizr placeholder polyfill
-(function($, window, document, app, undefined) {
-	app.modernizrPlaceholder = (function() {
-		var module = {};
-
-		var $placeholder = $('[placeholder]');
-
-		module.init = function() {
-			if(!Modernizr.input.placeholder){
-
-				$placeholder.focus(function() {
-					var input = $(this);
-			  		if (input.val() == input.attr('placeholder')) {
-						input.val('');
-						input.removeClass('placeholder');
-					}
-				}).blur(function() {
-					var input = $(this);
-					if (input.val() == '' || input.val() == input.attr('placeholder')) {
-						input.addClass('placeholder');
-						input.val(input.attr('placeholder'));
-					}
-				}).blur();
-
-				$placeholder.parents('form').submit(function() {
-					$(this).find('[placeholder]').each(function() {
-						var input = $(this);
-						if (input.val() == input.attr('placeholder')) {
-							input.val('');
-						}
-					})
-				});
-			}
-		};
-
-		return module;
-	})();
-})(jQuery, window, window.document, window.App);
-
-// Smooth scroll
-(function($, window, document, app, undefined) {
-    app.smoothScroll = (function() {
-        var module = {};
-
-        module.init = function() {
-            smoothScroll.init({
-                speed: 1000,
-                easing: 'easeInOutCubic',
-                offset: 0,
-                updateURL: false,
-            });
-        };
-
-        return module;
-    })();
-})(jQuery, window, window.document, window.App);
-
-
-
 jQuery(document).ready(function() {
     window.App.init();
 });
-
--- content/themes/TARVA/footer.php
+++ content/themes/TARVA/footer.php
@@ -26,10 +26,8 @@
 			</div>
 		</div>
 	</div>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyACDnzNTtrrrmVodkeqsgWROfyzc5xrMRA&sensor=true"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/scripts.min.js"></script>
-	<?php wp_footer(); ?>
 </body>
 </html>
