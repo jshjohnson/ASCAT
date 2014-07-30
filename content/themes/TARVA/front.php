b3b4074c057bf3a2f3d2ae85bcff89d6c446b9b9
Josh Johnson <josh@jshjohnson.com>
2014-07-30 20:26:19 +0100

    Minor fixes

--- content/themes/TARVA/front.php
+++ content/themes/TARVA/front.php
@@ -5,9 +5,9 @@ Template Name: Home
 ?>
 <?php get_header(); ?>	
 				<?php if ( have_posts() ) : ?>
-				<div class="header-video">
+<!-- 				<div class="header-video">
 					<iframe width="560" height="315" src="//www.youtube.com/embed/qas5lWp7_R0?&controls=0&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>
-				</div>
+				</div> -->
 				<section class="intro">
 					<div class="container">
 				<?php while ( have_posts() ) : the_post(); ?>
