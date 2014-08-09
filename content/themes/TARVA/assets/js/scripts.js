be762b9171fa37d33e96ca2d547138a1525553f3
Josh Johnson <josh@jshjohnson.com>
2014-08-09 13:25:54 +0100

    JS restructuring

--- content/themes/TARVA/assets/js/scripts.js
+++ content/themes/TARVA/assets/js/scripts.js
@@ -3,50 +3,14 @@ $(document).ready(function() {
     $("p:empty").remove();
     $(".wp-caption").removeAttr("style");
     $(".wp-content img, .wp-post-image, .wp-post-thumb").removeAttr("width").removeAttr("height");
-});
 
-(function($, window, document, undefined) {
+    var $svgImage = $('img[src*="svg"]');
 
-    'use strict';
-    window.App = (function() {
-        var app = {};
-
-        var _init = 0;
-
-        app.init = function() {
-            if (_init++) {
-                return;
-            }
-
-            app.modernizrSVG.init();
-
-        };
-
-        return app;
-    })();
-})(jQuery, window, window.document);
-
-// Modernizr SVG image polyfill
-(function($, window, document, app, undefined) {
-    app.modernizrSVG = (function() {
-        var module = {};
-
-        var $svgImage = $('img[src*="svg"]');
-
-        module.init = function() {
-            if(Modernizr.svg) {
-                return;
-            } else {
-                $svgImage.attr('src', function() {
-                    return $(this).attr('src').replace('.svg', '.png');
-                });
-            }
-        };
-
-        return module;
-    })();
-})(jQuery, window, window.document, window.App);
-
-jQuery(document).ready(function() {
-    window.App.init();
+    if(Modernizr.svg) {
+        return;
+    } else {
+        $svgImage.attr('src', function() {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
 });
